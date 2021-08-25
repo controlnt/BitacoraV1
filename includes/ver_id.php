51243ea1d01265c565c5823ee7abf55ee0006ac3
Calderon <juancalde130908@gmail.com>
2021-08-25 16:28:43 -0500

    se puede traer el id del usuario

--- includes/ver_id.php
+++ includes/ver_id.php
@@ -1,14 +1,15 @@
 
 <?php
+include("../bd.php");
 
 if(!isset($_SESSION)) 
 { 
         session_start(); 
 }
+
 $usuario = $_SESSION['usuario'];
 $query = "SELECT * FROM users WHERE usuario = '$usuario'";
 $resultados_id = mysqli_query($conectarbd, $query);
 $fila = mysqli_fetch_array($resultados_id);
-echo $usuario;
-
+echo $fila['id'];
 ?>
