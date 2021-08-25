e795492adca4d34a80920931cdaf092aa50faae0
Calderon <juancalde130908@gmail.com>
2021-08-25 17:44:22 -0500

    Revert "se puede traer el id del usuario"
    This reverts commit 51243ea1d01265c565c5823ee7abf55ee0006ac3.

--- includes/ver_id.php
+++ includes/ver_id.php
@@ -1,15 +1,14 @@
 
 <?php
-include("../bd.php");
 
 if(!isset($_SESSION)) 
 { 
         session_start(); 
 }
-
 $usuario = $_SESSION['usuario'];
 $query = "SELECT * FROM users WHERE usuario = '$usuario'";
 $resultados_id = mysqli_query($conectarbd, $query);
 $fila = mysqli_fetch_array($resultados_id);
-echo $fila['id'];
+echo $usuario;
+
 ?>
