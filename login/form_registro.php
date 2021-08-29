9b4c4adea67ed599d3a824eb08c229c59651c931
Calderon <juancalde130908@gmail.com>
2021-08-29 18:48:02 -0500

    nuevos estilos formulario registro

--- login/form_registro.php
+++ login/form_registro.php
@@ -3,13 +3,19 @@ $cerrar_session = "ocultar";
 include("../includes/header.php");?>
 
 <center>
-<div>
-    <h1>Registar</h1>
+<h1 class="form-control fs-1">Registro</h1>
+<div class="row">
         <form method="POST" action="registro.php">
-            <input type="text" name="usuario" id="usuario" placeholder="Ingrese Usuario" required>
-            <input type="email" name="correo" placeholder="Ingrese Correo">
-            <input type="password" name="contraseña" placeholder="Ingrese Contraseña" required>
-            <input type="submit" value="Registrar" name="btnregistrar">
+            <div class="col-2 m-1">
+            <input type="text" name="usuario" id="usuario" placeholder="Ingrese Usuario" required class="form-control">
+            </div>
+            <div class="col-2 m-1">
+            <input type="email" name="correo" placeholder="Ingrese Correo" class="form-control">
+            </div>
+            <div class="col-2 m-1">
+            <input type="password" name="contraseña" placeholder="Ingrese Contraseña" required class="form-control">
+            </div>
+            <input class="btn btn-success" type="submit" value="Registrar" name="btnregistrar">
         </form>
 </div>
 </center>
