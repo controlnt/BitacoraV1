406b478dcbd571128073bb19be65bdc773330124
Calderon <juancalde130908@gmail.com>
2021-11-05 22:22:49 -0500

    buscar.php con consultas mysqli POO pero sin probar

--- funciones/buscar.php
+++ funciones/buscar.php
@@ -1,18 +1,113 @@
 <?php
 
-include('../bd.php');
+//INCLUIMOS LOS ARCHIVOS DE LA VASE DE DATOS Y EL HEADER
+include("../bd.php");
+include("../includes/header.php");
 
+//VERIFICAR SI EXISTE UNA SESSION
 if (!isset($_SESSION)) {
     session_start();
 }
-if (isset($_POST['buscar_tarea'])) {
-    $fecha_i = $_POST['fecha_i'];
-    $fecha_f = $_POST['fecha_f'];
-    //$query = "SELECT * FROM task WHERE /*title like '$tarea_buscar' OR*/ date_t BETWEEN '". $fecha_i. "' AND '".$fecha_f."'";
-    $query = "SELECT * FROM task WHERE date_t BETWEEN '". $fecha_i. "' AND '".$fecha_f."'";
-    $result = mysqli_query($conectarbd, $query);
-    $_SESSION['t_buscadas'] = $result;
-    header("Location: formularios/form_buscar.php");
-}
+
+?>
+
+<div class="container p-4">
+    <h1>Consular Tareas</h1>
+        <hr>
+            <form class="container" method="POST" action="<?php htmlspecialchars($_SERVER['PHP_SELF']); ?>">
+                <div class="row align-items-center">
+                    <div class="col-1">
+                        <label for="desde">Desde</label>
+                    </div>
+                    <div class="col-4">
+                        <input type="datetime-local" name="fecha_i" class="form-control">
+                    </div>
+                    <div class="col-1">
+                        <label for="hasta">Hasta</label>
+                    </div>
+                    <div class="col-4">
+                        <input type="datetime-local" name="fecha_f" class="form-control">
+                    </div>
+                    <div class="col-2">
+                        <input type="submit" value="Buscar" class="btn btn-success" name="buscar_tarea">
+                    </div>
+                </div>
+            </form class="container">
+        <table class="table table-hover">
+            <thead>
+                <th>Titulo</th>
+                <th>Descripcion</th>
+                <th>Fecha</th>
+                <th>Estado</th>
+                <th>Acciones</th>
+            </thead>
+
+            <?php
+
+                if (isset($_POST['buscar_tarea'])) {
+
+                    //TRAER DATOS A CONSULTAR
+                    $fecha_i = $_POST['fecha_i'];
+                    $fecha_f = $_POST['fecha_f'];
+
+                    //CREAR Y PREPARAR EL QUERY
+                    $query = "SELECT id, title, description, estado, date_t FROM task WHERE date_t BETWEEN ? AND ? /*'". $fecha_i. "' AND '".$fecha_f."'*/";
+                    $stmt = $bd->prepare($query);
+
+                    //ASOSAR DATOS AL QUERY
+                    $exito = $stmt->bind_param("ss", $fecha_i, $fecha_f);
+
+                    //EJECUTAMOS EL QUERY
+                    $exito = $stmt->execute();
+
+                    //VERIFICAMOS QUE NO AYA ERRORES
+                    if ($exito==false) {
+                        echo "Error en la consulta";
+                    }else {
+                        //ASOCIAMOS LOS DATOS DEBUELTOS CON VARIABLES
+                        $exito = $stmt->bind_result($id, $titulo, $descripcion, $estado, $fecha);
+
+                        //IMPRIMIMOS LOS DATOS
+                        while ($stmt->fetch()) {
+
+            ?>
+                <tbody>
+                    <tr>
+                        <td><?= $titulo ?></td>
+                        <td><?= $descripcion ?></td>
+                        <td><?= $fecha ?></td>
+                        <td><?= $estado ?></td>
+                        <td>
+                            <a href="eliminar.php?id=<?= $id ?>" class="btn btn-danger">
+                                <i class="fa fa-trash-alt"></i>
+                            </a>
+                            <a href="editar.php?id=<?= $id ?>" class="btn btn-secondary">
+                                <i class="fa fa-marker"></i>
+                            </a>
+                                <a href="realizada.php?id=<?= $id ?>" class="btn btn-success">
+                                    <i class="fas fa-check"></i>
+                            </a>
+                        </td>
+                    </tr>
+                </tbody>
+                            
+            <?php
+
+            }
+            //CERRAMOS LA SENTENCIA
+            $stmt->close();
+        }
+    }
+
+            ?>
+
+        </table>
+</div>
+
+
+<?php
+
+//INCLUR EL PIE DE PAGINA
+include("../includes/footer.php")
 
 ?>
