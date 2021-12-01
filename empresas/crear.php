58d59e778f0367c9d65bdb36a64dbd6ba2760636
Calderon <juancalde130908@gmail.com>
2021-12-01 16:37:38 -0500

    se pueden crear empresas y se agrega automaticamente al Id_empresa del usuario

---
+++ empresas/crear.php
@@ -0,0 +1,444 @@
+<?php
+
+include('../includes/header.php');
+include('../bd.php');
+include('../includes/ver_login.php');
+
+?>
+
+<div class="container p-4">
+
+    <form action="crear.php" method="post" class="row justify-content-cente">
+
+        <div class="col">
+            <input type="text" name="nombre" placeholder="Nombre De Empresa" class="form-control">
+        </div>
+
+        <div class="col">
+            <input type="number" name="nit" placeholder="Nit De La Empresa" class="form-control">
+        </div>
+
+        <div class="col">
+            <input type="submit" name="btn_crear" value="Crear" class="btn btn-success">
+        </div>
+
+    </form>
+
+</div>
+
+<?php
+
+/*if (isset($_POST['btn_crear'])) {
+
+    $nombre = $_POST['nombre'];
+    $nit = $_POST['nit'];
+
+    $query = "SELECT codigo FROM empresas WHERE codigo BETWEEN '10000' AND '99999' ORDER BY codigo DESC LIMIT 1";
+    $stmt = $bd->prepare($query);
+
+    if (!$stmt) {
+
+        echo "Error en la preparacion del query: (" . $bd->errno . ")" . $bd->error;
+
+    }else {
+
+        $exito = $stmt->execute();
+
+        if (!$exito) {
+
+            echo "Error en la ejecucion: (" . $stmt->errno . ")" .  $stmt->error;
+
+        }else {
+
+            $exito = $stmt->bind_result($codigo);
+
+            if (!$exito) {
+
+                echo "Error en la vinculacion de resultados1: (" . $stmt->errno . ")" . $stmt->error;
+
+            }else {
+
+                $exito = $stmt->fetch();
+
+                if (!$exito) {
+
+                    echo "Error en la vinculacion de resultados con la variables: (" . $stmt->errno . ")" . $stmt->error;
+
+                }else {
+
+                    $stmt->close();
+
+                    $codigo_s = $codigo + 1;
+
+                    $query = "SELECT codigo FROM empresas WHERE codigo = ? LIMIT 1";
+                    $stmt = $bd->prepare($query);
+
+                    if (!$stmt) {
+
+                        echo "Error en la preparacion: (" . $bd->errno . ")" . $bd->error;
+
+                    }else {
+
+                        $exito = $stmt->bind_param('i', $codigo_s);
+
+                        if (!$exito) {
+
+                            echo "Error en la vinculacion de parametros: (" . $stmt->errno . ")" . $stmt->error;
+
+                        }else {
+
+                            $exito = $stmt->execute();
+
+                            if (!$exito) {
+
+                                echo "Error en la ejecuion: (" . $stmt->errno . ")" . $stmt->error;
+
+                            }else {
+
+                                $exito = $stmt->bind_result($codigo_d);
+
+                                if (!$exito) {
+
+                                    echo "Error en la asociacion de resultados: (" . $stmt->errno . ")" . $stmt->error;
+
+                                }else {
+
+                                    $exito = $stmt->fetch();
+
+                                    if (!$exito) {
+
+                                        echo "Error en la vinculacion de resultados2: (" . $stmt->errno . ")" . $stmt->error;
+                                        $stmt->close();
+
+                                    }else if ($exito == NULL) {
+
+                                        $stmt->close();
+
+                                        $query = "INSERT INTO empresas(empresa, nit, codigo) VALUES (?, ?, ?)";
+                                        $stmt->prepare($query);
+
+                                        if (!$exito) {
+
+                                            echo "Error en la preparacion: (" . $bd->errno . ")" . $bd->error;
+
+                                        }else {
+
+                                            $exito = $stmt->bind_param('sii', $nombre, $nit, $codigo);
+
+                                            if (!$exito) {
+
+                                                echo "Error en la vinculacion de parametros: (" . $stmt->errno . ")" . $stmt->error;
+
+                                            }else {
+
+                                                $exito = $stmt->execute();
+
+                                                if (!$exito) {
+
+                                                    echo "Error en la ejecuion: (" . $stmt->errno . ")" . $stmt->error;
+
+                                                }
+
+                                            }
+
+                                        }
+
+                                    }else {
+
+                                        $query = "SELECT codigo FROM empresas WHERE codigo BETWEEN '10000' AND '99999' ORDER BY codigo ASC LIMIT 1";
+                                        $stmt = $bd->prepare($query);
+
+                                        if (!$stmt) {
+
+                                            echo "Error en la preparacion del query: (" . $bd->errno . ")" . $bd->error;
+
+                                        }else {
+
+                                            $exito = $stmt->execute();
+
+                                            if (!$exito) {
+
+                                                echo "Error en la ejecuion: (" . $stmt->errno . ")" . $stmt->error;
+
+                                            }else {
+
+                                                $exito = $stmt->bind_result($codigo_2);
+
+                                                if (!$exito) {
+
+                                                    echo "Error en la vinculacion de resultados3: (" . $stmt->errno . ")" . $stmt->error;
+
+                                                }else {
+
+                                                    $stmt->fetch();
+
+                                                    $codigo_r = $codigo_2 - 1;
+
+                                                    $query = "INSERT INTO empresas(empresa, nit, codigo) VALUES (?, ?, ?)";
+                                                    $stmt = $bd->prepare($query);
+
+                                                    if (!$stmt) {
+
+                                                        echo "Error en la preparacion del query: (" . $bd->errno . ")" . $bd->error;
+
+                                                    }else {
+
+                                                        $exito = $stmt->bind_param('sii', $nobmre, $nit, $codigo_r);
+
+                                                        if (!$exito) {
+
+                                                            echo "Error en la vinculacion de parametros: (" . $stmt->errno . ")" . $stmt->error;
+
+                                                        }else {
+
+                                                            $exito = $stmt->execute();
+
+                                                            if (!$exito) {
+
+                                                                echo "Error en la ejecuion: (" . $stmt->errno . ")" . $stmt->error;
+
+                                                            }
+
+                                                        }
+
+                                                    }
+
+                                                }
+
+                                            }
+
+                                        }
+
+                                    }
+
+                                }
+
+                            }
+
+                        }
+
+                    }
+
+                }
+
+            }
+
+        }
+
+    }
+
+}*/
+
+if (isset($_POST['btn_crear'])) {
+
+    $nombre = $_POST['nombre'];
+    $nit = $_POST['nit'];
+
+    if (!$stmt = $bd->prepare("SELECT codigo FROM empresas WHERE codigo BETWEEN '10000' AND '99999' ORDER BY codigo DESC LIMIT 1")) {
+
+        die("Error en la preparacion del query: (" . $bd->errno . ")" . $bd->error);
+
+    }
+
+    if (!$exito = $stmt->execute()) {
+
+        die("Error en la ejecucion: (" . $stmt->errno . ")" .  $stmt->error);
+
+    }
+
+    if (!$exito = $stmt->bind_result($codigo_e)) {
+
+        die("Error en la vinculacion de resultados1: (" . $stmt->errno . ")" . $stmt->error);
+
+    }
+
+    $stmt->fetch();
+
+    $codigo_s = $codigo_e + 1;
+
+    $stmt->close();
+
+    if (!$stmt = $bd->prepare("SELECT codigo FROM empresas WHERE codigo = ? LIMIT 1")) {
+
+        die("Error en la preparacion del query: (" . $bd->errno . ")" . $bd->error);
+
+    }
+
+    if (!$exito = $stmt->bind_param("i", $codigo_s)) {
+
+        die("Error en la vinculacion de parametros: (" . $stmt->errno . ")" . $stmt->error);
+
+    }
+
+    if (!$exito = $stmt->execute()) {
+
+        die("Error en la ejecucion: (" . $stmt->errno . ")" .  $stmt->error);
+
+    }
+
+    if (!$exito = $stmt->bind_result($codigo_v)) {
+
+        die("Error en la vinculacion de resultados1: (" . $stmt->errno . ")" . $stmt->error);
+
+    }
+
+    $stmt->fetch();
+
+    $stmt->close();
+
+    if ($codigo_v == NULL) {
+
+        $codigo_n = true;
+
+    }
+
+    if ($codigo_v == !NULL) {
+
+        $codigo_n = false;
+
+    }
+
+    if (isset($codigo_n)&&($codigo_n)) {
+
+        if (!$stmt = $bd->prepare("INSERT INTO empresas(empresa, nit, codigo) VALUES (?, ?, ?)")) {
+    
+            die("Error en la preparacion del query: (" . $bd->errno . ")" . $bd->error);
+    
+        }
+    
+        if (!$exito = $stmt->bind_param("sii", $nombre, $nit, $codigo_s)) {
+    
+            die("Error en la vinculacion de parametros: (" . $stmt->errno . ")" . $stmt->error);
+    
+        }
+    
+        if (!$exito = $stmt->execute()) {
+    
+            die("Error en la ejecucion: (" . $stmt->errno . ")" .  $stmt->error);
+    
+        }
+    
+        $stmt->close();
+
+        $codigo_f = $codigo_s;
+
+    }
+
+
+    if (isset($codigo_n)&&(!$codigo_n)) {
+
+        if (!$stmt = $db->prepare("SELECT codigo FROM empresas WHERE codigo BETWEEN '10000' AND '99999' ORDER BY codigo ASC LIMIT 1")) {
+    
+            die("Error en la preparacion del query: (" . $bd->errno . ")" . $bd->error);
+    
+        }
+    
+        if (!$exito = $stmt->execute()) {
+    
+            die("Error en la ejecucion: (" . $stmt->errno . ")" .  $stmt->error);
+    
+        }
+    
+        if (!$exito = $stmt->bind_result($codigo_2)) {
+    
+            die("Error en la vinculacion de resultados1: (" . $stmt->errno . ")" . $stmt->error);
+    
+        }
+    
+        $stmt->fetch();
+    
+        $stmt->close();
+    
+        $codigo_r = $codigo_2 - 1;
+    
+        if (!$stmt = $bd->prepare("INSERT INTO empresas(empresa, nit, codigo) VALUES (?, ?, ?)")) {
+    
+            die("Error en la preparacion del query: (" . $bd->errno . ")" . $bd->error);
+    
+        }
+    
+        if (!$exito = $stmt->bind_param("sii", $nombre, $nit, $codigo_r)) {
+    
+            die("Error en la vinculacion de parametros: (" . $stmt->errno . ")" . $stmt->error);
+    
+        }
+    
+        if (!$exito = $stmt->execute()) {
+    
+            die("Error en la ejecucion: (" . $stmt->errno . ")" .  $stmt->error);
+    
+        }
+    
+        $stmt->close();
+
+        $codigo_f = $codigo_r;
+    
+    }
+
+    if (!$stmt = $bd->prepare("SELECT id FROM empresas WHERE codigo = ?")) {
+
+        die("Error en la preparacion del query: (" . $bd->errno . ")" . $bd->error);
+
+    }
+
+    if (!$stmt->bind_param("i", $codigo_f)) {
+
+        die("Error en la vinculacion de parametros: (" . $stmt->errno . ")" . $stmt->error);
+
+    }
+
+    if (!$stmt->execute()) {
+
+        die("Error en la ejecucion: (" . $stmt->errno . ")" .  $stmt->error);
+
+    }
+
+    if (!$stmt->bind_result($id_e)) {
+
+        die("Error en la vinculacion de resultados1: (" . $stmt->errno . ")" . $stmt->error);
+
+    }
+
+    $stmt->fetch();
+
+    $stmt->close();
+
+    $id_u = $_SESSION['id'];
+
+    if (!$stmt = $bd->prepare("UPDATE users SET id_empresas = ? WHERE id = ?")) {
+
+        die("Error en la preparacion del query: (" . $bd->errno . ")" . $bd->error);
+
+    }
+
+    if (!$stmt->bind_param("ii", $id_e, $id_u)) {
+
+        die("Error en la vinculacion de parametros: (" . $stmt->errno . ")" . $stmt->error);
+
+    }
+
+    if (!$stmt->execute()) {
+
+        die("Error en la ejecucion: (" . $stmt->errno . ")" .  $stmt->error);
+
+    }
+
+    $stmt->close();
+
+    $_SESSION['tipo-mensaje'] = "success";
+    $_SESSION['mensaje'] = "EMPRESA CREADA CORRECTAMENTE CODIGO: " . $codigo_f;
+    
+    header("Location: ../index.php");
+
+}
+
+unset($codigo_n);
+unset($codigo_2);
+unset($codigo_s);
+unset($codigo_r);
+unset($codigo_f);
+unset($id_e);
+
+include('../includes/footer.php');
+
+?>
